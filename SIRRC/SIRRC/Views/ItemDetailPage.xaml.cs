001b363d4a31117d075f7ce04533f2fd7e52669a
Tran Huy <trdtranduchuy@gmail.com>
2021-05-02 22:29:40 +0700

    Add project files.

---
+++ SIRRC/SIRRC/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SIRRC.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SIRRC.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
